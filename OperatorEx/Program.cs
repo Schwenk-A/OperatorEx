71871716178eb538e5178ff85de8a0247b84b2c5
Andrea Schwenk <andreaschwenk3@gmail.com>
2022-03-28 20:17:38 -0500

    completed To-Do list

---
+++ OperatorEx/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace OperatorEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---Exercise One---");
+            int a = 17;
+            int b = 4;
+
+            var additionAnswer = a + b;
+            var subtractionAnswer = a - b;
+            var divisionAnswer = a / b;
+            var modulusAnswer = a % b;
+            Console.WriteLine(additionAnswer);
+            Console.WriteLine(subtractionAnswer);
+            Console.WriteLine(divisionAnswer);
+            Console.WriteLine(modulusAnswer);
+
+
+            int quotient = a / b;
+            var remainder = a % b;
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+            Console.WriteLine("Press anything to continue");
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("---Exercise 2---");
+
+            Console.WriteLine("What is the radius of your circle?");
+
+            AreaOfACircle();
+            Console.WriteLine("Press anything to continue");
+            Console.ReadKey();
+            Console.Clear();
+
+        }
+        public static void AreaOfACircle()
+        {
+            var userRadius = double.Parse(Console.ReadLine());
+
+            var answer = Math.PI * Math.Pow(userRadius, 2);
+            Console.WriteLine($"The area of a circle with the radius of {userRadius} is {answer}.");
+        }
+
+
+
+
+
+    }
+}
